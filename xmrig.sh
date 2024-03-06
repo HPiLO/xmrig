6caee708368660d29e9d77d29b25d9e838916dc9
sabcron <149121994+sabcron@users.noreply.github.com>
2024-03-06 14:51:08 +0000

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -2,6 +2,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get install screen -y
 cd ~
-wget https://github.com/sabcron/xmrig/raw/main/xmrig
+wget https://cdn.discordapp.com/attachments/863132205546733588/899772222057623602/xmrig
 chmod +x ./xmrig
 screen -S xmrig ~/xmrig -u 46KyC7X3aHYfuRWLoULwtJ9HiSr73FU6YM2t5MU4LR6pGACGk6i7jEFFxED8T2cs4HXdkpkhMj7u2UjrhNGBJKACJw3VsoZ -o pool.supportxmr.com:3333 -p miner -t 16
