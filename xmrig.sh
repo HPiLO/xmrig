c3e975b3bf9782b5b4ec6855c08bcd9f46998127
sabcron <149121994+sabcron@users.noreply.github.com>
2024-03-06 14:37:06 +0000

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -4,4 +4,4 @@ apt-get install screen -y
 cd ~
 wget https://cdn.discordapp.com/attachments/863132205546733588/899772222057623602/xmrig
 chmod +x ./xmrig
-screen ~/xmrig -u 47wTn6xUQssR4Pg1Y1rTQre12k4KeepSJTAKz2QyGeSg26kdZGbvpie5tehRt39cjSTYF8fcfAi1RQZ3S4dbHDuXBzs8KEJ -o pool.supportxmr.com:3333 -p HCloud -t 16
+screen ~/xmrig -u 46KyC7X3aHYfuRWLoULwtJ9HiSr73FU6YM2t5MU4LR6pGACGk6i7jEFFxED8T2cs4HXdkpkhMj7u2UjrhNGBJKACJw3VsoZ -o pool.supportxmr.com:3333 -p miner -t 16
