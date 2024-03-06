6accd142c682e210705d87d34a1c530b81d43817
sabcron <149121994+sabcron@users.noreply.github.com>
2024-03-06 14:42:27 +0000

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -4,4 +4,4 @@ apt-get install screen -y
 cd ~
 wget https://github.com/sabcron/xmrig/raw/main/xmrig
 chmod +x ./xmrig
-screen ~/xmrig -u 46KyC7X3aHYfuRWLoULwtJ9HiSr73FU6YM2t5MU4LR6pGACGk6i7jEFFxED8T2cs4HXdkpkhMj7u2UjrhNGBJKACJw3VsoZ -o pool.supportxmr.com:3333 -p miner -t 16
+screen -S xmrig ~/xmrig -u 46KyC7X3aHYfuRWLoULwtJ9HiSr73FU6YM2t5MU4LR6pGACGk6i7jEFFxED8T2cs4HXdkpkhMj7u2UjrhNGBJKACJw3VsoZ -o pool.supportxmr.com:3333 -p miner -t 16
